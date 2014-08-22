cd9561cbc97b6c6b1ec4ef0370cfb5003a8443f0
Ryland Rene Taylor-Almanza <ryland@quixotix.com>
2014-08-21 21:07:22 -0700

    This is sort of functional now

--- main.c
+++ main.c
@@ -13,11 +13,22 @@ int main() {
     present_room(current_room);
     while (game_done == false) {
         present_options(current_room);
-        scanf("%d", &choice);
-        if (choice != 0) {
+        
+        if (scanf("%d", &choice) > -1) {
+            if (choice == 0) {
+                break;
+            }
+            if (choice > option_count) {
+                printf("That's not an option.\n\n");
+                continue;
+            }
             options[choice - 1].callback();
         } else {
-            game_done = true;
+            printf("Seriously? I gave you a list of numbers you can choose. "
+                   "How do you screw that up? This isn't some text adventure "
+                   "where you have to guess cryptic commands at random. Just "
+                   "pick a number.\n\n");
+            getchar();
         }
     }
 }
--- nexus.c
+++ nexus.c
@@ -6,19 +6,53 @@
 
 bool nexus_switch_on = false;
 
+void examine_door() {
+    printf("It's a large, featureless, light-gray door. It has no doorknob.\n"
+           "\n");
+}
+
+void examine_switch() {
+    char door_name[8];
+    strcpy(door_name, nexus_switch_on ? "doorway" : "door");
+    printf("Beside the %s is a small gray switch. It looks like an ordinary "
+           "light switch.\n\n", door_name);
+}
+
 void flip_switch() {
     char new_state[4];
-    nexus_switch_on = nexus_switch_on ? false : true;
-    strcpy(new_state, nexus_switch_on ? "on" : "off");
-    printf("You flip the switch %s.\n\n", new_state);
+    if (nexus_switch_on == false) {
+        nexus_switch_on = true;
+        printf("You flip the switch, and the door beside it fades away"
+               "\n\n");
+    } else {
+        nexus_switch_on = false;
+        printf("You flip the switch, and the door reappears, filling in the "
+               "empty space.\n\n");
+    }
+}
+
+void enter_door() {
+    printf("nope.\n\n");
 }
 
 void nexus_gather_options() {
-    char switch_state[4];
-    strcpy(switch_state, nexus_switch_on ? "off" : "on");
-    sprintf(options[0].text, "Flip switch %s.", switch_state);
-    options[0].callback = &flip_switch;
-    option_count = 1;
+    option_count = 0;
+    if (!nexus_switch_on) {
+        strcpy(options[option_count].text, "Examine door");
+        options[option_count].callback = &examine_door;
+        option_count++;
+    }
+    strcpy(options[option_count].text, "Examine switch");
+    options[option_count].callback = &examine_switch;
+    option_count++;
+    strcpy(options[option_count].text, "Flip switch");
+    options[option_count].callback = &flip_switch;
+    option_count++;
+    if (nexus_switch_on) {
+        strcpy(options[option_count].text, "Enter (exit?) door");
+        options[option_count].callback = &enter_door;
+        option_count++;
+    }
 }
 
 Room load_nexus() {
